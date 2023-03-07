5a0fee8ba40914dfa2aa9c34e90e38c969b27338
eliel15000 <eliezercoding@gmail.com>
2023-03-06 21:50:36 -0500

    model and repository updates

--- user-mngt-system/src/main/java/com/eliezercoding/usersystem/model/User.java
+++ user-mngt-system/src/main/java/com/eliezercoding/usersystem/model/User.java
@@ -1,11 +1,25 @@
 package com.eliezercoding.usersystem.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
+    @Column(name = "first_name")
     private String firstName;
+
+    @Column(name = "last_name")
     private String lastName;
+
+    @Column(name = "email_id")
     private String emailId;
+
+    @Column(name = "role")
     private String role;
 
     public User() {}
