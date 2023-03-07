927a24e77803dece4caaf586c4e2fe046d09ccaa
eliel15000 <eliezercoding@gmail.com>
2023-03-06 22:02:15 -0500

    exception update

--- user-mngt-system/src/main/java/com/eliezercoding/usersystem/controller/UserController.java
+++ user-mngt-system/src/main/java/com/eliezercoding/usersystem/controller/UserController.java
@@ -1,9 +1,29 @@
 package com.eliezercoding.usersystem.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.eliezercoding.usersystem.model.User;
+import com.eliezercoding.usersystem.repository.UserRepository;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+@CrossOrigin(origins = "#####################")
 @RestController
-@RequestMapping("#######")
+@RequestMapping("#############")
 public class UserController {
+
+    UserRepository userRepository;
+
+    // C - create user
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return this.userRepository.save(user);
+    }
+
+    // R - get all users
+    @GetMapping
+    public List<User> getAllUsers() {
+        return this.userRepository.findAll();
+    }
+
+
 }
