7bc487f8d88d95bd382e1c13dc21a3235c7002bd
eliel15000 <eliezercoding@gmail.com>
2023-03-06 22:18:33 -0500

    controller update

--- user-mngt-system/src/main/java/com/eliezercoding/usersystem/controller/UserController.java
+++ user-mngt-system/src/main/java/com/eliezercoding/usersystem/controller/UserController.java
@@ -1,7 +1,9 @@
 package com.eliezercoding.usersystem.controller;
 
+import com.eliezercoding.usersystem.exception.ResourceNotFoundException;
 import com.eliezercoding.usersystem.model.User;
 import com.eliezercoding.usersystem.repository.UserRepository;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -25,5 +27,33 @@ public class UserController {
         return this.userRepository.findAll();
     }
 
+    // R - get user by id
+    @GetMapping("/{id}")
+    public User getUserById(@PathVariable(value = "id") long userId) {
+        return this.userRepository.findById(userId)
+                .orElseThrow(() -> new ResourceNotFoundException("User not found with id: " + userId));
+    }
 
+    // U - update user
+    @PutMapping("/{id}")
+    public User updateUser(@RequestBody User userNewDetails, @PathVariable("id") long userId) {
+        User existingUser = this.userRepository.findById(userId)
+                .orElseThrow(() -> new ResourceNotFoundException("User not found with id: " + userId));
+
+        existingUser.setFirstName(userNewDetails.getFirstName());
+        existingUser.setLastName(userNewDetails.getLastName());
+        existingUser.setEmailId(userNewDetails.getEmailId());
+
+        return this.userRepository.save(existingUser);
+    }
+
+    // D - delete user by id
+    @DeleteMapping("/{id}")
+    public ResponseEntity<User> deleteUser(@PathVariable("id") long userId) {
+        User userToDelete = this.userRepository.findById(userId)
+                .orElseThrow(() -> new ResourceNotFoundException("User not found with id: " + userId));
+
+        this.userRepository.delete(userToDelete);
+        return ResponseEntity.ok().build();
+    }
 }
