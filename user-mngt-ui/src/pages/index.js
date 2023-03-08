51783eaeece524b5551dd7904e864fb191503bf6
eliel15000 <eliezercoding@gmail.com>
2023-03-07 22:26:19 -0500

    tailwind added

--- user-mngt-ui/src/pages/index.js
+++ user-mngt-ui/src/pages/index.js
@@ -18,7 +18,7 @@ export default function Home() {
       </Head>
 
       <main>
-        <h1>HI</h1>
+        <h1 className=" text-center py-[30%]">{process.env.NEXT_PUBLIC_HI}</h1>
       </main>
     </>
   )
